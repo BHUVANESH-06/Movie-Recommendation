3df01432e5ac4eb753c3a41efcc88ab3bde565b5
Bhuvanesh <bhuvanesh.g666@gmail.com>
2024-10-02 04:08:31 +0530

    first commit

--- frontend/src/Top.js
+++ frontend/src/Top.js
@@ -18,7 +18,7 @@ function Top({ genre }) {
     useEffect(() => {
         const getRecommendations = async () => {
             try {
-                const response = await axios.get('https://movie-recommendation-system-ag2i.onrender.com/api/top', {
+                const response = await axios.get('http://localhost:5003/api/top', {
                     params: { genre }
                 });
                 const movieTitles = response.data;
