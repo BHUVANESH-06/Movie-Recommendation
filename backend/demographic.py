677c90af8776e7157931eb8ab7fefd05c955be18
Bhuvanesh <bhuvanesh.g666@gmail.com>
2024-10-02 07:34:29 +0530

    Shabba2

--- backend/demographic.py
+++ backend/demographic.py
@@ -1,3 +1,12 @@
+import pkg_resources
+
+# Check installed packages
+installed_packages = pkg_resources.working_set
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
+print(installed_packages_list)
+
+
+
 import pandas as pd
 import numpy as np
 import sys
